50535d5969310b1e1f37b2379a9fb1e0a03ca213
Marissa L. Lewinski <marissa.lewinski18@kzoo.edu>
2021-10-05 10:21:51 -0400

    Hehe title is unearth now

--- mario/main.lua
+++ mario/main.lua
@@ -24,7 +24,7 @@ require 'src/Dependencies'
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.graphics.setFont(gFonts['medium'])
-    love.window.setTitle('Super 50 Bros.')
+    love.window.setTitle('Unearth')
 
     math.randomseed(os.time())
     
